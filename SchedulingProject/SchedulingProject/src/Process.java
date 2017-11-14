a8c10bcf32ddd8e75e3b07b588e2a77b790b0893
Yevgeniy <yshatrovskiy@gmail.com>
2017-11-14 08:48:06 -0500

    first

---
+++ SchedulingProject/SchedulingProject/src/Process.java
@@ -0,0 +1,65 @@
+
+public class Process {
+	
+	static int timeCreated;
+	int runningTime;
+	int cpuTime;
+	boolean cpuBound;
+	int firstCPUBurst;
+	
+	public Process(int timeCreated, int cpuTime, boolean cpuBound) {
+		super();
+		this.timeCreated = timeCreated;
+		this.cpuTime = cpuTime;
+		this.cpuBound = cpuBound;
+		
+		if(cpuBound){
+			this.firstCPUBurst = (4000/2) + (int) (Math.random() * 4000);
+		}else{
+			this.firstCPUBurst = 20000/2 + (int) (Math.random() * 20000);
+		}
+	}
+
+	public int getTimeCreated() {
+		return timeCreated;
+	}
+
+	public void setTimeCreated(int timeCreated) {
+		this.timeCreated = timeCreated;
+	}
+
+	public int getCpuTime() {
+		return cpuTime;
+	}
+
+	public void setCpuTime(int cpuTime) {
+		this.cpuTime = cpuTime;
+	}
+
+	public boolean isCpuBound() {
+		return cpuBound;
+	}
+
+	public void setCpuBound(boolean cpuBound) {
+		this.cpuBound = cpuBound;
+	}
+
+	public int getFirstCPUBurst() {
+		return firstCPUBurst;
+	}
+
+	public void setFirstCPUBurst(int firstCPUBurst) {
+		this.firstCPUBurst = firstCPUBurst;
+	}
+	
+	public int getRunningTime() {
+		return runningTime;
+	}
+
+	public void setRunningTime(int runningTime) {
+		this.runningTime = runningTime;
+	}
+
+	
+
+}
